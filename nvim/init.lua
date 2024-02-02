b63fae6916c3a1216da9850febe61376400521e1
Fin Gentzen <87946264+fgtubs@users.noreply.github.com>
2024-02-02 13:45:39 +0000

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,7 +4,5 @@ require("plugins")
 vim.cmd('colorscheme catppuccin-frappe')
 
 vim.api.nvim_exec([[
-autocmd VimEnter * silent! rightbelow vnew ~/.config/nvim/nvim-cheatsheet.md | vertical resize 30 | wincmd h
-autocmd VimEnter * setlocal buftype=nofile bufhidden=wipe nobuflisted noswapfile
-autocmd VimEnter * setlocal filetype=markdown
+autocmd VimEnter * silent! rightbelow vnew ~/.config/nvim/nvim-cheatsheet.md | setlocal buftype=nofile bufhidden=wipe nobuflisted noswapfile | setlocal filetype=markdown | vertical resize 30 | wincmd h
 ]], false)
